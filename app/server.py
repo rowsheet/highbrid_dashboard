a3b56bb93cf0f77a9c9d0c74190a67831e27eae3
Alexander Kleinhans <alexanderkleinhans@protonmail.com>
2019-09-07 10:36:46 -0600

    First commit.

---
+++ app/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template, jsonify
+import json
+import os
+import requests
+import base64
+app = Flask(__name__,template_folder="templates")
+
+@app.route("/", methods=["GET", "POST"])
+def v1_charges():
+        return render_template("pages/home.html", foo="Foo")
+
+@app.route("/ping", methods=["POST", "GET"])
+def ping():
+        print("got ping")
+        return "pong"
+
+if __name__ == "__main__":
+        app.run(
+                host= "0.0.0.0",
+                port=5400,
+                debug=True
+        )
